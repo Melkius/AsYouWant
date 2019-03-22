e124488dd9085cfb6da39f4cda2526d42ca20aa7
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-03-22 13:31:11 +0100

    ft/ added navigation with args to detailled activity and btn to go back

--- App.js
+++ App.js
@@ -1,15 +1,78 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Image,
+  TouchableOpacity
+} from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class HomeScreen extends React.Component {
+  state = {
+    search: "",
+    cards: [],
+    isLoading: false
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  fetchTheApi = concat => {
+    const url = `https://api.elderscrollslegends.io/v1/cards`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data =>
+        concat
+          ? this.setState(state => ({
+              cards: state.cards.concat(data.cards)
+            }))
+          : this.setState(() => ({
+              cards: data.cards
+            }))
+      )
+      .then(() => this.setState({ isLoading: false }))
+      .catch();
+  };
+
+  onDetail = item => {
+    this.props.navigation.navigate("Details", { card: item });
+  };
+
+  componentDidMount() {
+    this.fetchTheApi();
+  }
+
   render() {
+    const { search } = this.state;
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("Details")}
+        <FlatList
+          numColumns="2"
+          showsVerticalScrollIndicator
+          data={this.state.cards}
+          renderItem={({ item }) => {
+            console.log(item);
+            return (
+              <TouchableOpacity onPress={() => this.onDetail(item)}>
+                <Image
+                  style={{
+                    flex: 1,
+                    margin: 5,
+                    minWidth: 180,
+                    maxWidth: 250,
+                    height: 304,
+                    maxHeight: 304
+                  }}
+                  source={{ uri: item.imageUrl }}
+                  key={item.index}
+                />
+              </TouchableOpacity>
+            );
+          }}
         />
       </View>
     );
@@ -18,12 +81,18 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
   render() {
+    const { navigation } = this.props;
+    const card = navigation.getParam("card", "No-Object");
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
+        <Text>{card.name}</Text>
+        <Image
+          source={{ uri: card.imageUrl }}
+          style={{ width: 150, height: 200 }}
+        />
         <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.navigate("Home")}
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
         />
       </View>
     );
